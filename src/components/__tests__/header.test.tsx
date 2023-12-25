2a0b3fbe86f15afa37c67dd4a0586159349481cb
Mauricio Girardi <maurigirarde@yahoo.com.br>
2023-12-25 18:57:14 +0000

    config test

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -1,8 +1,34 @@
-import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { describe, expect, it, vi } from "vitest";
+
+import { Header } from "../header";
+
+vi.mock("react-router-dom", async () => {
+  const actual = await vi.importActual("react-router-dom");
+  return {
+    ...actual,
+    useLocation: () => {
+      return {
+        pathname: "localhost:3000/",
+      };
+    },
+  };
+});
 
 describe("Header", () => {
   it("should render correctly", () => {
-    // TODO create all tests
-    expect(2).toBe(2);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    expect(
+      screen.getByRole("link", { name: /dashboard/i }),
+    ).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /pedidos/i })).toBeInTheDocument();
+    expect(screen.getByText(/pedidos/i)).toHaveAttribute("href", "/orders");
+    expect(screen.getByText(/dashboard/i)).toHaveAttribute("href", "/");
   });
 });
--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,23 @@
+import { resolve } from "node:path";
+
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    include: ["**/*.{test,spec}.?(c|m)[jt]s?(x)"],
-    exclude: ["node_modules", "dist", ".idea", ".git", ".cache", "utils"],
-    // setupFiles: ['../.test/setup.js'],
+    exclude: [
+      "node_modules",
+      "dist",
+      ".idea",
+      ".git",
+      ".cache",
+      "../utils/**/*",
+    ],
+    // coverage: {
+    //   enabled: true,
+    // },
+    setupFiles: ["./.test/setup.js"],
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
 });
