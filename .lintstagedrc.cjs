6410ba73f5897bf74334c48ca437fc0c5002953e
Mauricio Girardi <maurigirarde@yahoo.com.br>
2023-12-26 13:43:36 +0000

    fix build

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,5 +3,6 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm run test ${filenames.join(' --file')}`,
   ]
 }
--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -1,34 +1,32 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import { describe, expect, it, vi } from "vitest";
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { describe, expect, it, vi } from 'vitest'
 
-import { Header } from "../header";
+import { Header } from '../header'
 
-vi.mock("react-router-dom", async () => {
-  const actual = await vi.importActual("react-router-dom");
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom')
   return {
     ...actual,
     useLocation: () => {
       return {
-        pathname: "localhost:3000/",
-      };
+        pathname: 'localhost:3000/',
+      }
     },
-  };
-});
+  }
+})
 
-describe("Header", () => {
-  it("should render correctly", () => {
+describe('Header', () => {
+  it('should render correctly', () => {
     render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>,
-    );
+    )
 
-    expect(
-      screen.getByRole("link", { name: /dashboard/i }),
-    ).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: /pedidos/i })).toBeInTheDocument();
-    expect(screen.getByText(/pedidos/i)).toHaveAttribute("href", "/orders");
-    expect(screen.getByText(/dashboard/i)).toHaveAttribute("href", "/");
-  });
-});
+    expect(screen.getByRole('link', { name: /dashboard/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /pedidos/i })).toBeInTheDocument()
+    expect(screen.getByText(/pedidos/i)).toHaveAttribute('href', '/orders')
+    expect(screen.getByText(/dashboard/i)).toHaveAttribute('href', '/')
+  })
+})
